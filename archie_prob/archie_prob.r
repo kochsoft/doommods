d822db1f28ef3b880e9951161531b32a89538f26
Markus-Hermann Koch <mhk@markuskoch.eu>
2020-07-20 00:06:08 +0200

    feat(stat): Found that 0.0313==8/255.
    This is quite suggestive as to how the roll works. Either needing
    a value <=8 or >=247 on the roll table. Adjusted the script accordingly.

--- archie_prob/archie_prob.r
+++ archie_prob/archie_prob.r
@@ -1,37 +1,40 @@
-# Dedicated to decino and caused by his Arch-Vile video
+# Dedicted to decino and caused by his Arch-Vile video
 # https://www.youtube.com/watch?v=xsVWdMVYJgk&t=40
 
 # This is for seeing probabilities to stun the arch-vile.
 
 # Assumptions:
 # * Rational numbers are always truncated to integers prior to comparisons with rng.
-# * Let r in rng in {0,..,255}. A probability p in [0,1] is assumed to be
-#   met for 'forward==TRUE' if floor(256*(1-p)) <= r
-#   else for 'forward == FALSE' if r < floor(256*p).
+# * Let r in rng in {0,..,255}. Seeing that 8/255 equals 0.0313 quite precisely, I assume
+#   that we either need a value <= 8 or >= 247 for an Arch-Vile pain state.
+#   I label the former with the property forward == FALSE and the latter forward == TRUE.
+#   Note that this is imperfect: A threshhold of 255 would still be liable to pain if
+#   255 is rolled. However, using 256 as maximum would miss the 0.0313 that is neatly
+#   matched with 8/255 using 255 as denominator.
 # * As stated in the video the probability to enter its pain state
-#   is assumed (&t=40) to be 0.0313.
+#   is assumed (&t=40) to be 0.0313 == 8/255.
 # * A strike consists of so many consecutive hits. 7 for a shotgun,
 #   20 for the super shotgun, 1 for a pistol.
 
 # Textual Results:
 # ================
 # Concerning Super Shotguns:
-# > disp_pain(rng, p_pain, n_pellets=20, forward=TRUE );
-#   20 pellets, p_pain is 0.0313. Forward probabilities (pain state requires high numbers): Pain count 165, Pain prob 0.6445.
-# > disp_pain(rng, p_pain, 20, FALSE);
-#   20 pellets, p_pain is 0.0313. Backward probabilities (pain state requires low numbers): Pain count 130, Pain prob 0.5078.
+# > disp_pain(rng, thresh_pain, 20, TRUE );
+#   20 pellets, thresh_pain is 247. Forward probabilities (pain state requires high numbers): Pain count 165, Pain prob 0.6445.
+# > disp_pain(rng, thresh_pain, 20, FALSE);
+#   20 pellets, thresh_pain is 9. Backward probabilities (pain state requires low numbers): Pain count 131, Pain prob 0.5117.
 #
 # Concerning Zombie Shotguns:
-# > disp_pain(rng, p_pain,  7, TRUE );
-#   7 pellets, p_pain is 0.0313. Forward probabilities (pain state requires high numbers): Pain count 71, Pain prob 0.2773.
-# > disp_pain(rng, p_pain,  7, FALSE);
-#   7 pellets, p_pain is 0.0313. Backward probabilities (pain state requires low numbers): Pain count 49, Pain prob 0.1914.
+# > disp_pain(rng, thresh_pain,  7, TRUE );
+#   7 pellets, thresh_pain is 247. Forward probabilities (pain state requires high numbers): Pain count 71, Pain prob 0.2773.
+# > disp_pain(rng, thresh_pain,  7, FALSE);
+#   7 pellets, thresh_pain is 9. Backward probabilities (pain state requires low numbers): Pain count 50, Pain prob 0.1953.
 #
 # Concerning Trusty Pistol:
-# > disp_pain(rng, p_pain,  1, TRUE );
-#   1 pellets, p_pain is 0.0313. Forward probabilities (pain state requires high numbers): Pain count 12, Pain prob 0.0469.
-# > disp_pain(rng, p_pain,  1, FALSE);
-#   1 pellets, p_pain is 0.0313. Backward probabilities (pain state requires low numbers): Pain count 7, Pain prob 0.0273.
+# > disp_pain(rng, thresh_pain,  1, TRUE );
+#   1 pellets, thresh_pain is 247. Forward probabilities (pain state requires high numbers): Pain count 12, Pain prob 0.0469.
+# > disp_pain(rng, thresh_pain,  1, FALSE);
+#   1 pellets, thresh_pain is 9. Backward probabilities (pain state requires low numbers): Pain count 8, Pain prob 0.0312.
 
 # https://www.doomworld.com/forum/topic/89069-shotgun-minimum-damage-and-rng/
 rng = c(0, 8, 109, 220, 222, 241, 149, 107,  75, 248, 254, 140,  16,  66 ,
@@ -57,23 +60,26 @@ rng = c(0, 8, 109, 220, 222, 241, 149, 107,  75, 248, 254, 140,  16,  66 ,
 # Does a hit of so many pellets lead to a pain-state?
 #  rng: The rng table.
 #  index0: The position in the table the rng pointer points at right now.
-#  p_pain: Probability of entering a pain-state. Decino: "Archie has 0.0313".
+#  thresh_pain: Value in {0,..,255} for determining when a pain state should
+#    be entered. Is a low or a high value considered "good resistance"?
+#    depends on 'forward parameter' below. Decino: "Archie has p==0.0313 for pain".
+#    [Note that restricting the values to 255 instead of 256 will either lead
+#     to "perfect resistance" or "perfect sensitivity" being impossible. I choose
+#     this limit because 8/255 meets decino's probability of 0.0313 so neatly.]
 #  n_pellets: Number of pellets served in the strike examined.
 #  forward: See comments above. Is a random experiment set up thus that
-#    success would require the rolling of high or of low numbers?
+#    success would require the rolling of high or of low numbers? TRUE means 'high'.
+#    I do not know, but for me it feels right that forward==TRUE is the gospel. Emotionally.
 # returns TRUE if the given parameters would lead to a pain state. FALSE else.
-is_painful <- function(rng, index0, p_pain, n_pellets, forward)
+is_painful <- function(rng, index0, thresh_pain, n_pellets, forward)
 {
   index = ((index0:(index0+n_pellets-1))%%length(rng)) + 1; # << R vectors are indexed starting at 1.
-  thresh_pain = NULL;
   ouch = NULL;
   if (forward)
   {
-    thresh_pain = floor((1.-p_pain) * 256.);
     ouch = sum(rng[index] >= thresh_pain) > 0;
   } else {
-    thresh_pain = floor(p_pain * 256.);
-    ouch = sum(rng[index] < thresh_pain) > 0;
+    ouch = sum(rng[index] <= 255-thresh_pain) > 0;
   }
   return (ouch);
 }
@@ -82,13 +88,13 @@ is_painful <- function(rng, index0, p_pain, n_pellets, forward)
 # check if the described hit-scenario would lead to a pain state.
 # returns the number of cases when this was observed. Since the RNG table has
 # 256 entries this value/256 is the probability for a pain-state.
-pain_count <- function(rng, p_pain, n_pellets, forward)
+pain_count <- function(rng, thresh_pain, n_pellets, forward)
 {
   N = length(rng);
   ouchs = 0;
   for (j in 1:N)
   {
-    if (is_painful(rng, j, p_pain, n_pellets, forward))
+    if (is_painful(rng, j, thresh_pain, n_pellets, forward))
     {
       ouchs = ouchs + 1;
     }
@@ -97,26 +103,26 @@ pain_count <- function(rng, p_pain, n_pellets, forward)
 }
 
 # Uses above functions to generate textual output.
-disp_pain <- function(rng, p_pain, n_pellets, forward)
+disp_pain <- function(rng, thresh_pain, n_pellets, forward)
 {
-  pc = pain_count(rng, p_pain, n_pellets, forward);
+  pc = pain_count(rng, thresh_pain, n_pellets, forward);
   pp = pc/length(rng);
   tp = if (forward) "Forward" else "Backward";
   hilo = if (forward) "high" else "low";
-  cat(sprintf("  %d pellets, p_pain is %1.4f. %s probabilities (pain state requires %s numbers): Pain count %d, Pain prob %1.4f.\n", n_pellets, p_pain, tp, hilo, pc, pp));  
+  thresh_pain_adj = if (forward) thresh_pain else (256-thresh_pain);
+  cat(sprintf("  %d pellets, thresh_pain is %d. %s probabilities (pain state requires %s numbers): Pain count %d, Pain prob %1.4f.\n", n_pellets, thresh_pain_adj, tp, hilo, pc, pp));
 }
 
-
-p_pain = 0.0313;
+thresh_pain = 247; # 255-8. Note that 8/255 == 0.0313 being the probability given by decino.
 
 cat("\nConcerning Super Shotguns:\n")
-disp_pain(rng, p_pain, 20, TRUE );
-disp_pain(rng, p_pain, 20, FALSE);
+disp_pain(rng, thresh_pain, 20, TRUE );
+disp_pain(rng, thresh_pain, 20, FALSE);
 
 cat("\nConcerning Zombie Shotguns:\n")
-disp_pain(rng, p_pain,  7, TRUE );
-disp_pain(rng, p_pain,  7, FALSE);
+disp_pain(rng, thresh_pain,  7, TRUE );
+disp_pain(rng, thresh_pain,  7, FALSE);
 
 cat("\nConcerning Trusty Pistol:\n")
-disp_pain(rng, p_pain,  1, TRUE );
-disp_pain(rng, p_pain,  1, FALSE);
+disp_pain(rng, thresh_pain,  1, TRUE );
+disp_pain(rng, thresh_pain,  1, FALSE);
